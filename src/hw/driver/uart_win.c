869dcca5d46dd91a1b7042d952e097d3839f51cb
SK Kim <a01055756383@gmail.com>
2026-08-07 15:24:24 +0900

    day03_complete_uart

--- src/hw/driver/uart_win.c
+++ src/hw/driver/uart_win.c
@@ -3,10 +3,30 @@
 #include <conio.h>
 #include <stdio.h>
 
+#define RX_BUF_SIZE 256
 
 static HANDLE hStdout = INVALID_HANDLE_VALUE;
 static HANDLE hStdin = INVALID_HANDLE_VALUE;
 
+static uint8_t rx_buf[RX_BUF_SIZE];
+static uint16_t rx_head = 0;
+static uint16_t rx_tail = 0;
+
+static void rx_push(uint8_t c){
+    uint16_t next=(rx_head + 1) % RX_BUF_SIZE; // %RX.. 원형 큐로 만드는? 256 넘어갔을때
+    if(next != rx_tail) {
+        rx_buf[rx_head] = c;
+        rx_head = next;
+    }
+}
+static bool rx_pop(uint8_t *p_data) {
+    if(rx_head == rx_tail) {
+        return false;
+    }
+    *p_data = rx_buf[rx_tail];
+    rx_tail = (rx_tail + 1) % RX_BUF_SIZE;
+    return true;
+}
 
 bool uartInit(void)
 {
@@ -19,7 +39,8 @@ bool uartInit(void)
         mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING | ENABLE_PROCESSED_OUTPUT;
         SetConsoleMode(hStdout, mode);
     }
-
+    rx_head = 0;
+    rx_tail = 0;
     return true;
 }
 
@@ -42,6 +63,35 @@ int uartWrite(uint8_t ch, uint8_t *p_data, uint32_t length)
 
 bool uartReadBlock(uint8_t ch, uint8_t *p_data, uint32_t timeout)
 {
-    
+    if (rx_pop(p_data)) {
+        return true;
+    }
+    int c = _getch();
+    if(c == 0x00 || c == 0xE0) {
+        int key = _getch();
+        rx_push('\x1B');
+        rx_push('[');
+        switch(key) {
+            case 72 : rx_push('A'); break; // up
+            case 80 : rx_push('B'); break; // down
+            case 75 : rx_push('C'); break; // left
+            case 77 : rx_push('D'); break; // right
+            default : break;
+        }
+        return rx_pop(p_data);
+    }
+    else if(c == '\r') { // enter
+        *p_data = '\r';
+        return true;
+    }
+    else if(c == 3) { // ctrl + c
+        *p_data = 0x03;
+        return true;
+    }
+    else {
+        *p_data = (uint8_t)c;
+        return true;
+    }
+
     return false;
 }
