2569c84aae7d488f5f34262a1396021811d5cb51
SK Kim <a01055756383@gmail.com>
2026-08-10 17:53:44 +0900

    0810_redPill_END

--- redpill/reverse_Mirroring.c
+++ redpill/reverse_Mirroring.c
@@ -29,31 +29,46 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void input(unsigned int num) {
-    uint8_t buf[8];
+int count = 0;
 
+void input(uint16_t num) {
+		// reverse 후 2진수 -> 16진수 변경을 위한 저장용 배열
+    char buf[9]; 
+    uint16_t temp = 0;
+
+    printf("Case %d:\n",++count);
     // 2진수 출력 for문
-    printf("Input : 0x%.2X ( ",num);
+    printf("\tInput\t: 0x%.2X ( ",num);
     for (int i = 8; i > 0; i--) {
         int result = (num >> i-1) & 1;
         printf("%d", result);
         if ( i%4 == 1 ) printf(" ");
     }
-    printf(")");
-    
-    printf("\n");
 
-    // 2진수 역순출력 for문
-    printf("Output : 0x%.2X ( ",num);
-    for (int i = 1; i <= 8; i++) {
-        int result = (num >> i-1) & 1;
-        printf("%d", result);
-        if ( i%4 == 0 ) printf(" ");
-    }
-    printf(")\n");
-    printf("Verify : OK\n");
-    printf("---------------\n");
+    printf(")");
+    printf("\n");
     
+    // Input 반전 후 배열저장
+    for (int i = 0; i < 8; i++) {
+        int result = (num >> i) & 1;
+        buf[i] = result + '0';
+        temp = (temp << 1) | (buf[i] - '0');
+    }
+    buf[8] = '\0'; // 배열 종료 인식용 
+
+    printf("\tOutput\t: 0x%.2X ( ", temp);
+
+    // 역순으로 저장된 배열 출력
+    for (int i = 0; i < 8; i++) {
+        printf("%c",buf[i]);
+        if (i % 4 == 3)
+        printf(" ");
+    }
+
+    printf(")\n");
+    printf("\tVerify\t: OK\n");
+    printf("--------------------------------------\n");
+
 }
 
 
